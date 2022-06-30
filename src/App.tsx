1f0eb344c9081f76420b9292be616f6023ab0d52
ammaxc <ammateur.xc@gmail.com>
2022-06-30 17:18:59 +0800

    feat: 新增组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.scoped.scss";
-
+import React from "react";
+import CustomHeader from "./components/Header/Header";
 function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("this is some error");
-  });
-  const str = "is vscode better than webstorm?";
-  useLayoutEffect(() => {
-    console.log(str);
-  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomHeader />
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.scoped.scss";
+const CustomHeader: React.FC = () => {
+  return <span className="wrapper">
+    this is react app
+  </span>;
+};
+export default CustomHeader;
