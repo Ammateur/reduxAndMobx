fe4c59bb3900b44424968c9d54f6e3f3918d4055
ammaxc <ammateur.xc@gmail.com>
2022-06-30 23:21:34 +0800

    feat: eslint&&prettier配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import CustomHeader from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <CustomHeader />
+        app
     </div>
   );
 }
