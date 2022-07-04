9afda4e1804a4609d6f3dce267ae09f8a386c72e
ammaxc <ammateur.xc@gmail.com>
2022-07-04 18:56:54 +0800

    feat: 引入antd

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "antd";
 
 function Login() {
-  return <div>this is log page</div>;
+  return (
+    <div>
+      this is log page
+      <Button>click me to login</Button>
+    </div>
+  );
 }
 
 export default Login;
